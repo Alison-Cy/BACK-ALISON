ffdb037bd38ce154c5e7a20fb185f40db489d5b8
Alison-Cy <avc.cumbajin@yavirac.edu.ec>
2024-11-03 15:57:30 -0500

    migraciones

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,12 +7,12 @@ export const databaseProvider= [
         inject: [ConfigService],
         useFactory:(config: ConfigService)=>{
             const dataSource= new DataSource({
-                type: 'postgres',
-                host: config.get('HOST'),
+                type:'postgres',
+                host:config.get('HOST') ||'localhost',
                 port: +config.get('PORT_DB'),
-                username: config.get('USERNAME'),
-                password: config.get('PASSWORD'),
-                database: config.get('DATABASE')
+                username: config.get('USERNAME') ||'root',
+                password: config.get('PASSWORD') || 'prueba',
+                database: config.get('DATABASE') 
                 
             });
             return dataSource.initialize();
